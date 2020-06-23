ee1490643856053ddd2d6ce05b3decb643159803
Bob D'Errico <bobderrico80@gmail.com>
2020-06-22 22:45:55 -0400

    Deploy bingo

---
+++ bingo/precache-manifest.94aa67ac4bb6f762fa1535c187a054d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294d61f4dddf136040ec7273f75bb37b",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "34350114ac378a52b844",
+    "url": "/bingo/static/css/main.e6e4fdd0.chunk.css"
+  },
+  {
+    "revision": "3ee0db993e4011186a86",
+    "url": "/bingo/static/js/2.5c69636e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bingo/static/js/2.5c69636e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34350114ac378a52b844",
+    "url": "/bingo/static/js/main.318b34a9.chunk.js"
+  },
+  {
+    "revision": "1e4257647166f81f55e3",
+    "url": "/bingo/static/js/runtime-main.a536a95e.js"
+  },
+  {
+    "revision": "e72962be22ed05e7b57d80076caa53a2",
+    "url": "/bingo/static/media/pride-bg.e72962be.jpg"
+  }
+]);
